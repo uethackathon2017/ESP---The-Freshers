acaefd3d3cd366e34898c420a96de6154748f127
Le Duc Thien <machina4785@gmail.com>
2017-03-10 18:57:41 +0700

    1st Hour
    1st Hour

---
+++ MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    //[SerializeField]
+    //private GameObject playButton,leaderBoardButton,settingButton,instructionButton;
+
+    [SerializeField]
+    private GameObject mainPanel,instructionPanel;
+
+
+    public void _playButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void _settingButton()
+    {
+        mainPanel.SetActive(false);
+    }
+
+    public void _leaderBoardButton()
+    {
+        mainPanel.SetActive(false);
+    }
+
+    public void _instructionButton()
+    {
+        mainPanel.SetActive(false);
+        instructionPanel.SetActive(true);
+    }
+
+    public void _backButton()
+    {
+        mainPanel.SetActive(true);
+        instructionPanel.SetActive(false);
+    }
+}
