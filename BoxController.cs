57b57773e2bf919db86a80bcb443b78e242cabb6
Le Duc Thien <machina4785@gmail.com>
2017-03-11 00:02:29 +0700

    5th commit
    5th commit

---
+++ BoxController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+	public static BoxController instance;
+	public int status;
+	private float minDistance = 50.0f;
+	Vector2 begin, end;
+
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	public void _SwipeDetect ()
+	{
+		if (Input.GetMouseButtonDown (0) == true) {
+			begin = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp (0) == true) {
+			end = Input.mousePosition;
+			if (begin != end && begin != Vector2.zero && end != Vector2.zero) {
+				Vector2 delta = end - begin;
+				if (Mathf.Abs (delta.x) > minDistance && Mathf.Abs (delta.x) > Mathf.Abs (delta.y)) {
+					status = (delta.x > 0) ? 1 : 2;
+				}
+				if (Mathf.Abs (delta.y) > minDistance && Mathf.Abs (delta.x) < Mathf.Abs (delta.y)) {
+					status = (delta.y > 0) ? 3 : 4;
+				}
+			}
+		}
+	}
+}
---
+++ LevelController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelController : MonoBehaviour {
+
+    public static LevelController instance;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        _makeInstance();
+        if (ScoreController.instance != null) ScoreController.instance.thisIsNewLevel = true;
+    }
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    void _makeInstance()
+    {
+        if (instance == null) instance = this;
+    }
+
+
+    public void _restartButton()
+    {
+        MapMaker.instance._BuildMap(MapMaker.instance.map);
+    }
+
+
+    public void _levelPassedButton()
+    {
+        MapMaker.instance.levelPassedPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void _gameOverButton()
+    {
+        MapMaker.instance.gameOverPanel.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+
+    public int _checkendgame()
+    /* m = 0 <=> thua
+     * m = 1 <=> thắng
+     */
+    {
+        int i, j;
+        int m = 1;
+        for (i = 0; i < 15; i++)
+        {
+            for (j = i + 1; j < 16; j++)
+            {
+                if (MapMaker.instance.listText[i].text != MapMaker.instance.listText[j].text)
+                {
+                    m = 0;
+                    break;
+                }
+                else continue;
+            }
+        }
+        return m;
+    }
+}
