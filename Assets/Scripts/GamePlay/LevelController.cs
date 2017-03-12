b716c87a435a2a44d095028e0a00c6a026ea0b68
Le Duc Thien <machina4785@gmail.com>
2017-03-12 07:19:56 +0700

    Toàn bộ project
    Đây là toàn bộ project của nhóm em. Do bọn em nghĩ chỉ cần commit những
    file code .cs và file dữ liệu .json nên mới commit muộn như thế này,
    mong BTC thông cảm

---
+++ Assets/Scripts/GamePlay/LevelController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelController : MonoBehaviour {
+
+    public static LevelController instance;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        _makeInstance();
+    }
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    void _makeInstance()
+    {
+        if (instance == null) instance = this;
+    }
+
+
+    public void _restartButton()
+    {
+        MapMaker.instance._BuildMap(MapMaker.instance.map);
+    }
+
+
+    public void _levelPassedButton()
+    {
+        MapMaker.instance.levelPassedPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void _gameOverButton()
+    {
+        SceneManager.LoadScene(0);
+		//Time.timeScale = 1;
+    }
+
+
+    public int _checkendgame()
+    /* m = 0 <=> thua
+     * m = 1 <=> th?ng
+     */
+    {
+        int i, j;
+        int m = 1;
+        for (i = 0; i < 15; i++)
+        {
+            for (j = i + 1; j < 16; j++)
+            {
+                if (MapMaker.instance.listText[i].text != MapMaker.instance.listText[j].text)
+                {
+                    m = 0;
+                    break;
+                }
+                else continue;
+            }
+        }
+        return m;
+    }
+}
---
+++ Library/Collab/Base/Assets/Scripts/GamePlay/MapMaker.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DentedPixel;
+public class MapMaker : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip swipeClip;
+
+    public int countLevel = 1;
+    public static MapMaker instance;
+    private bool isMoving;
+	[SerializeField]
+	private GameObject pointer;
+
+    public GameObject levelPassedPanel;
+
+	public List<Text> listText = new List<Text>();
+
+	public int[,] map ;
+
+	private int x = 0, y = 0,pos = 0;
+
+	private Vector3 temp;
+
+	public List<GameObject> row = new List<GameObject> ();
+
+    public Text levelPassedText;
+
+    public GameObject gameOverPanel;
+
+	void Awake(){
+		if (DataExtractor.instance != null) {
+			DataExtractor.instance._ReadJSON (countLevel);
+			x = DataExtractor.instance.x;
+			y = DataExtractor.instance.y;
+			map = DataExtractor.instance.map;
+			pointer.transform.position = row [x * 4 + y].transform.position;
+			pos = x + y;
+		}
+		if (DataExtractor.instance != null) {
+			DataExtractor.instance._ReadJSON (countLevel);
+			_BuildMap (map);
+		}
+        _makeInstance();
+
+		Application.targetFrameRate = 30;		//Co dinh FPS bang 30
+		QualitySettings.vSyncCount = 2;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        isMoving = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		BoxController.instance._SwipeDetect ();
+		_MoveBox (BoxController.instance.status);
+        levelPassedText.text = "Level " + (countLevel-1) + " Passed";
+        if (LevelController.instance._checkendgame() == 1)
+        {
+            levelPassedPanel.SetActive(true);
+            Time.timeScale = 0;
+
+            countLevel += 1;
+            DataExtractor.instance._ReadJSON(countLevel);
+            x = DataExtractor.instance.x;
+            y = DataExtractor.instance.y;
+            map = DataExtractor.instance.map;
+            pointer.transform.position = row[x * 4 + y].transform.position;
+            pos = x + y;
+            _BuildMap(map);
+        }
+
+        if (ScoreController.instance != null)
+           if (ScoreController.instance.stepRemaining == 0)
+           {
+               gameOverPanel.SetActive(true);
+               Time.timeScale = 0;
+           }
+    }
+
+    void _makeInstance()
+    {
+        if (instance == null) instance = this;
+    }
+
+
+
+    void _MoveBox(int status)
+    {
+        pos = x * 4 + y;
+        switch (status)
+        {
+            case 1:
+                {
+                    if (y < 3)
+                    {
+                        if (!isMoving)
+                        {
+                            isMoving = true;// ngan cac hoat dong khac trong khi dang di chuyen
+                            LeanTween.move(pointer, row[pos + 1].transform.position, .1f).setOnComplete(() =>
+                            {
+                                isMoving = false;
+                                y++;
+                                _ChangeNumber(x, y);
+                                audioSource.PlayOneShot(swipeClip);
+                            });// nho doan nay
+
+                        }
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    if (y > 0)
+                    {
+                        if (!isMoving)
+                        {
+                            isMoving = true;
+                            LeanTween.move(pointer, row[pos - 1].transform.position, .1f).setOnComplete(() =>
+                            {
+                                isMoving = false;
+                                y--;
+                                _ChangeNumber(x, y);
+                                audioSource.PlayOneShot(swipeClip);
+                            });// nho doan nay
+                        }
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    if (x>0)
+                    if (!isMoving)
+                    {
+                        isMoving = true;
+                        LeanTween.move(pointer, row[pos - 4].transform.position, .1f).setOnComplete(() =>
+                        {
+                            isMoving = false;
+                            x--;
+                            _ChangeNumber(x, y);
+                            audioSource.PlayOneShot(swipeClip);
+                        });// nho doan nay
+                    }
+                    break;
+                }
+            case 4:
+                {
+                    if(x<3)
+                    if (!isMoving)
+                    {
+                        isMoving = true;
+                        LeanTween.move(pointer, row[pos + 4].transform.position, .1f).setOnComplete(() =>
+                        {
+                            isMoving = false;
+                            x++; ;
+                            _ChangeNumber(x, y);
+                            audioSource.PlayOneShot(swipeClip);
+                        });// nho doan nay
+                    }
+                    break;
+                }
+        }
+        BoxController.instance.status = 0;
+    }
+
+    void _Add(Text text, int num)
+	{
+		text.text = "" + num;
+	}
+
+	public void _BuildMap(int[,] imap)
+	{
+		int i, j;
+		i = 0;
+		j = 0;
+		foreach (Text text in listText)
+		{
+			_Add(text, imap[i, j]);  //Add l?n lu?t theo th? t? t? tr�i qua ph?i, t? tr�n xu?ng du?i
+			if (j == 3)
+			{
+				i++;
+				j = 0;
+			}
+			else
+				j++;
+		}
+	}
+	void _ChangeNumber(int x,int y){
+		map [x, y]++;
+		listText [x * 4 + y].text = "" + map [x, y];
+	}
+}
